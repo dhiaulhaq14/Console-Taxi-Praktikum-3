0c01afc652ca75ad39a3a08358a68be4ab6c7a77
dhiaulhaq14 <zidkydhiaulhaq14@students.amikom.ac.id>
2022-03-24 21:05:33 +0700

    pertama commit

---
+++ ConsoleTaxi/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleTaxi
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty);
+            Console.WriteLine("Num Passenger : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Jono sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Jono sudah menurunkan penumpang", DriverName);
+        }
+    }
+}
